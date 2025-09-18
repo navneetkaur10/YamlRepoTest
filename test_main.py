32f532a3d78bc5c4bcbd7415e66750f5658c688b
navneet <navneetkaur10@rrc.ca>
2025-09-18 13:02:10 -0500

    corrected the file again

--- test_main.py
+++ test_main.py
@@ -5,7 +5,7 @@ def test_hello_returns_none():
 
 def test_bye_returns_none():
     assert bye() is None
-
+    
 
 def test_hello_prints_expected_text(capsys):
     hello()
@@ -15,4 +15,4 @@ def test_hello_prints_expected_text(capsys):
 def test_bye_prints_expected_text(capsys):
     bye()
     captured = capsys.readouterr()
-    assert captured.out.strip() == "GoodMorning!"
+    assert captured.out.strip() == "Goodbye!"
